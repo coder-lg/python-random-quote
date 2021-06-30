1ba5c4e8dc9d1793a86d669edf2825b63837513f
LG <76845820+coder-lg@users.noreply.github.com>
2021-06-30 14:44:21 +0530

    removing space

--- get-quote.py
+++ get-quote.py
@@ -11,8 +11,14 @@ def quote():
 
   last=27
   rnd=random.randint(0,last)
+  rnd2=random.randint(0,last)
+  rnd3=random.randint(0,last)
+  rnd4=random.randint(0,last)
 
-  print(quotes[rnd])
+  print(quotes[rnd], end = ' ')
+  print(quotes[rnd2], end = ' ')
+  print(quotes[rnd3], end = ' ')
+  print(quotes[rnd4], end = ' ')
 
 if __name__== "__main__":
   quote()
